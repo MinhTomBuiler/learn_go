cc78787e4cf2a8aff107f4c5bfc63ef9a7790cae
Jofen Quan <jofenquan@gmail.com>
2021-12-19 20:12:52 +0900

    commit source

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "fmt"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("HELLO WORLD"))
-
+	w.Write([]byte("HELLO WORLD"))
 
 }
-func serveVideo(w http.ResponseWriter, r *http.Request){
+func serveVideo(w http.ResponseWriter, r *http.Request) {
 
-    phanloai := r.URL.Query().Get("phanloai")
-    id := r.URL.Query().Get("id")
-    w.Header().Add("Content-Type","video/mp4")//dinh dang mp4 truoc
-    path := fmt.Sprintf("video/%s/%s.mp4", phanloai, id)
-    http.ServeFile(w,r, path)
+	phanloai := r.URL.Query().Get("phanloai")
+	id := r.URL.Query().Get("id")
+	w.Header().Add("Content-Type", "video/mp4") //dinh dang mp4 truoc
+	path := fmt.Sprintf("video/%s/%s.mp4", phanloai, id)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		w.Header().Add("Content-Type", "text/html; charset=UTF-8")
+
+		http.ServeFile(w, r, "static/a.html")
+
+		return
+
+	}
+
+	http.ServeFile(w, r, path)
 }
 
-func main () {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/abc",home)
-    mux.HandleFunc("/video",serveVideo)
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/abc", home)
+	mux.HandleFunc("/video", serveVideo)
 
-    log.Println("Server dang khoi dong...")
-    err := http.ListenAndServe(":4000", mux)//khai bao cong
-    log.Fatal(err)//xuat chu mau do
+	log.Println("Server dang khoi dong...")
+	err := http.ListenAndServe(":4000", mux) //khai bao cong
+	log.Fatal(err)                           //xuat chu mau do
 }
-
-    
